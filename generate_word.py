a910110d638082ece7a192974ff0850417f9084a
Benn Linger <benn@linger.com>
2016-08-30 18:51:34 -0400

    Implemented guaranteed uniqueness through DynamoDB conditional put.

--- generate_word.py
+++ generate_word.py
@@ -3,7 +3,9 @@
 """
 from __future__ import print_function
 import json
-import boto3
+import boto3, botocore
+
+max_tries = 5
 
 def lambda_handler(event, context):
     print("Event: {}".format(json.dumps(event)))
@@ -11,12 +13,28 @@ def lambda_handler(event, context):
     # Get reference to our SQS queue.
     queue = boto3.resource("sqs").Queue(event["wordlist-queue-url"])
     
+    # Get reference to our used words table.
+    used_words_table = boto3.resource("dynamodb").Table(event["used-words-table"])
+    
+    try_count = 0
+    while try_count < max_tries:
+        try_count += 1
+        
+        try:
+            return reserve_next_word(queue, used_words_table)
+        except Exception as e:
+            print("Error: {}".format(e))
+    
+    raise Exception("Unable to generate unique word.")
+
+def reserve_next_word(queue, used_words_table):
+    
     print("Pulling the next word from the word list queue.")
     
     sqs_message_list = queue.receive_messages()
     
     if len(sqs_message_list) == 0:
-        raise Exception("No words available in queue.")
+        raise StopIteration("No words available in queue.")
     
     sqs_message = sqs_message_list[0]
     
@@ -30,14 +48,6 @@ def lambda_handler(event, context):
     print("Deleting message from the queue.")
     sqs_message.delete()
     
-    print("Adding new message to queue with incremented count value.")
-    queue.send_message(
-        MessageBody = json.dumps({
-            "word": new_word,
-            "count": times_returned + 1
-        })
-    )
-    
     final_word = None
     
     if times_returned == 1:
@@ -46,6 +56,29 @@ def lambda_handler(event, context):
     else:
         # Append a number representing the number of times the word has been returned.
         final_word = "{}{:d}".format(new_word, times_returned)
+    
+    
+    # Ensure word hasn't already been reserved.
+    try:
+        used_words_table.put_item(
+            Item = {
+                "PartitionKey": final_word
+            },
+            ConditionExpression=boto3.dynamodb.conditions.Attr("PartitionKey").not_exists()
+        )
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+            raise AttributeError("Word pulled from queue ({}) already taken.".format(final_word))
+        else:
+            raise
+    
+    print("Adding new message to queue with incremented count value.")
+    queue.send_message(
+        MessageBody = json.dumps({
+            "word": new_word,
+            "count": times_returned + 1
+        })
+    )
     
     print("Returning word: \"{}\"".format(final_word))
     
